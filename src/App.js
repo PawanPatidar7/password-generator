1d3baff8a6a44b66f89d5eb702961ef331b3d558
Pawan Patidar <pawanpatidar@monocept.com>
2024-08-06 13:53:21 +0530

    password generator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useCallback, useEffect, useRef} from 'react'
 
 function App() {
+ const [length, setLength] = useState(8)
+ const [numberAllowed, setNumberAllowed] = useState(false)
+ const [charAllowed, setCharAllowed] = useState(false)
+ const [password, setPassword] = useState("")
+ 
+ const passwordRef = useRef(null)
+
+const passwordGenerator = useCallback(() => {
+    let pass="";
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "0123456789" 
+    if (charAllowed) str += "!@#$%^&*-_=+[]{}`~"
+    
+    for (let i = 1; i <= length; i++) {
+      let char= Math.floor(Math.random() * str.length + 1)
+        
+      pass += str.charAt(char)
+      
+    }
+    setPassword(pass)
+
+},[length, numberAllowed, charAllowed, setPassword])
+
+
+const copyPasswordToClipboard = useCallback(() => {
+  passwordRef.current?.select();
+  passwordRef.current?.setSelectionRange(0, 999);
+  window.navigator.clipboard.writeText(password)
+}, [password])
+
+useEffect(() => {
+  passwordGenerator()
+}, [length, numberAllowed, charAllowed, passwordGenerator])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="w-full max-w-md mx-auto h-40 shadow-md rounded-lg px-4 py-3 my-8 mt-40 bg-gray-600 text-orange-600">
+      <h1 className='text-white text-center my-3'>Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4 '>
+        <input
+             type="text"
+             value={password}
+             className='outline-none w-full py-1 px-3'
+             placeholder='Password'
+             readOnly
+             ref={passwordRef}
+             />
+           
+           <button 
+           onClick={copyPasswordToClipboard}
+               className='outline-none bg-blue-800 text-white px-3 py-0.5 shrink-0'>
+          Copy </button>
+
+      </div>
+     <div className='flex text-sm gap-2'>
+      <div className='flex item-center gap-x-1 font-bold'>
+       <input 
+           type='range'
+           min={6}
+           max={20}
+           value={length}
+           className='cursor-pointer'
+           onChange={(e) => {setLength(e.target.value)}}
+            />
+        <label >Length({length})</label>
+      </div>
+      <div className='flex item-center gap-x-1 font-bold'>
+       <input
+           type='checkbox'
+          defaultChecked={numberAllowed}
+          id="numberInput"
+           onChange={() => {setNumberAllowed((prev) => !prev)}}
+            />
+        <label htmlFor='numberInput'>Number</label>
+      </div>
+      <div className='flex item-center gap-x-1 font-bold'>
+       <input
+          type='checkbox'
+          defaultChecked={charAllowed}
+          id="characterInput"
+           onChange={() => {setNumberAllowed((prev) => !prev)}}
+            />
+        <label htmlFor='characterInput'>Characters</label>
+      </div>
+     </div>
+      </div>
+   
+    
+    </>
   );
 }
 
